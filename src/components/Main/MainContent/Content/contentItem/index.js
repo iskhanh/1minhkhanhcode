c3b8e5acd711623bb333abd202556b69b9116aa0
Nguyen Duy Khanh <you@example.com>
2023-02-01 02:51:54 +0700

    css

--- src/components/Main/MainContent/Content/contentItem/index.js
+++ src/components/Main/MainContent/Content/contentItem/index.js
@@ -1,6 +1,9 @@
 import classNames from 'classnames/bind';
 import Slider from 'react-slick';
 import { Link } from 'react-router-dom';
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
+
 
 import styles from './contents.module.scss';
 
